d5ca23136d18033c677f3d28d596f2f784eaa2fa
cristopherSantana6 <cristopherrafael644@gmail.com>
2025-05-13 09:58:41 -0600

    ejercicio2

--- ejercicio2.py
+++ ejercicio2.py
@@ -7,12 +7,12 @@
 total_mes = 0  # Inicializa el total acumulado del mes
 gastos_semanales = 4  # Número de semanas
 gastos_dia = 7  # Número de días por semana
-for semana in range(gastos_semanales):
-    print(f"\nSemana {semana + 1}:")
+for mes in range (1, 5):  # Itera sobre las semanas (1 a 4)
     total_semana = 0  # Inicializa el total de la semana
-    for dia in range(gastos_dia):
-        gasto_dia = float(input(f"Ingrese el gasto del día {dia + 1}: "))
-        total_semana += gasto_dia 
-        total_mes += total_semana  # Acumula el total del mes
-    print(f"Total gastado en la semana {semana + 1}: C${total_semana:.2f}")
-    print(f"Total acumulado del mes {total_mes:.2f}")
+    print(f"\nSemana {mes}:")
+    for dia in range(1, 8):  # Itera sobre los días (1 a 7)
+        gasto = float(input(f"Ingrese el gasto del día {dia}: "))  # Solicita el gasto del día
+        total_semana += gasto  # Suma el gasto al total de la semana
+    print(f"Total gastado en la semana {mes}: {total_semana:.2f}")  # Muestra el total de la semana
+    total_mes += total_semana  # Suma el total de la semana al total acumulado del mes
+print(f"\nTotal acumulado del mes: {total_mes:.2f}")  # Muestra el total acumulado del mes
