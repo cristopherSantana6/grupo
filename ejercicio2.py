194c26e85c2d235b7a71783a4a6356b8ddee4d2f
BaZa06 <bazeledon@uamv.edu.ni>
2025-05-13 12:01:32 -0600

    "Se realizo un archivo a la salida"

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,15 @@
 #Registro semanal de gastos de estudiantes UAM
 
-días 
+gastos_semanales = 4
+total_mes = 0
+gastos_día = 7
+for semana in range (1,):
+    print(f"\nSemana {semana + 1}: ")
+    total_semana = 0
+    for día in range (gastos_día):
+        gasto_día = float(input("Ingrese los datos del primer día: "))
+        total_semana += gasto_día
+        print(f"El monto total de los gastos enla semana {semana + 1}: C${total_semana:.2f}")
+else:        
+    total_mes += total_semana
+    print(f"El monto total de los gastos del mes es {total_mes:.2f}")
---
+++ ejercicio3.py
@@ -0,0 +1,18 @@
+#Cálculo de promedio académico en la UAM
+
+calificacionesEstudiantes_asignaturas = 3
+
+asignatura_tareas = 3
+
+asignatura_examen = 1
+
+cantidad_de_estudiantes = int(input("Ingrese el número de estudiantes de la carrera de ingeniería en sistemas: "))
+
+for estudiantes in range (1, cantidad_de_estudiantes + 1 ):
+    total_estudiantes = 0
+    print(f"\nTotal de estudiantes {estudiantes + 1}")
+    for asignaturas in range (1,3):
+        suma_notas = 
+        print(f"Suma de las notas {asignaturas + 1}")
+        
+        
