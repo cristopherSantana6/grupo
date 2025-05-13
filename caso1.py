ad66b58053e3e81015528931966d6c98e0e78a1a
andyrueda439 <ajrueda@uamv.edu.ni>
2025-05-13 08:40:11 -0500

    Vigoron

---
+++ caso1.py
@@ -0,0 +1,21 @@
+#Desarrolle un programa que permita simular la venta de vigorón durante una feria gastronómica organizada en la Universidad Americana, UAM Managua. El sistema debe solicitar la cantidad de clientes atendidos y, por cada cliente, cuántas porciones de vigorón compró, junto con el precio unitario por porción. El programa debe calcular el total pagado por cada cliente y mostrar el total de ventas de toda la feria. Utilice estructuras cíclicas anidadas.
+
+clientes_atendidos = int(input("Ingrese la cantidad de clientes atendidos: "))
+total_ventas = 0
+
+for cliente in range (clientes_atendidos):
+    print(f"\nCliente {cliente + 1}:")
+
+    porciones = int(input("Cuantas porciones de vigoron compro: "))
+
+    total_cliente = 0
+
+    for porcion in range (porciones):
+        precio_unitario = float(input(f"Ingrese el precio de la porcion #{porcion}: "))
+        total_cliente += precio_unitario
+    
+    print(f"Total a pagar por el cliente {cliente}: C${total_cliente:.2f}")
+
+    total_ventas += total_cliente
+
+print(f"\nTotal de ventas en toda la feria: C${total_ventas:.2f}")
